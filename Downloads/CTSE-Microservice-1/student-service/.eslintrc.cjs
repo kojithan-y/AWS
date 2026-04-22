49a4e35a5236844ddb7b117f5542ca4dae9c427f
Koji Yogarajah <139199365+kojithan-y@users.noreply.github.com>
2026-04-22 16:58:24 +0530

    Delete Downloads/CTSE-Microservice-1 directory

--- Downloads/CTSE-Microservice-1/student-service/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: { node: true, es2022: true, jest: true },
-  extends: ['eslint:recommended'],
-  parserOptions: { ecmaVersion: 'latest' },
-  rules: {
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-  },
-};
